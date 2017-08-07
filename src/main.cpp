e421d1ba824b753b861cfc4f91ac01f5319d6565
Sulabh Matele <sulabhmatele@outlook.com>
2017-08-06 23:24:59 -0400

    Update Udacity quiz codes

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,16 @@ int main() {
 /*    MatrixXd Xsig_aug = MatrixXd(15, 7);
     ukf.AugmentedSigmaPoints(&Xsig_aug);*/
 
-    MatrixXd Xsig_pred = MatrixXd(15, 5);
-    ukf.SigmaPointPrediction(&Xsig_pred);
+/*    MatrixXd Xsig_pred = MatrixXd(15, 5);
+    ukf.SigmaPointPrediction(&Xsig_pred);*/
+
+/*    VectorXd x_pred = VectorXd(5);
+    MatrixXd P_pred = MatrixXd(5, 5);
+    ukf.PredictMeanAndCovariance(&x_pred, &P_pred);*/
+
+    VectorXd z_out = VectorXd(3);
+    MatrixXd S_out = MatrixXd(3, 3);
+    ukf.PredictRadarMeasurement(&z_out, &S_out);
 
     return 0;
 }
--- src/ukf.h
+++ src/ukf.h
@@ -34,7 +34,6 @@ public:
     void PredictMeanAndCovariance(VectorXd* x_pred, MatrixXd* P_pred);
     void PredictRadarMeasurement(VectorXd* z_out, MatrixXd* S_out);
     void UpdateState(VectorXd* x_out, MatrixXd* P_out);
-
 };
 
 #endif /* UKF_H */
